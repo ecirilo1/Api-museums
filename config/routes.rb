e04712ac21763a14b1da71ec9b0d92ebc76cad80
Estelle <estelle.cirilo@gmail.com>
2021-09-10 14:59:03 +0200

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1
+  end
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
