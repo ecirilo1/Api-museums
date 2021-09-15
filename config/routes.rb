c971d5a6e7f01e0788f7f25317efc19f481fe21b
Estelle <estelle.cirilo@gmail.com>
2021-09-15 11:54:30 +0200

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   namespace :api, defaults: { format: :json } do
-    namespace :v1
+    namespace :v1 do
+      get 'museums', to: 'museums#search'
+    end
   end
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
